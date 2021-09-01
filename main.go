85478961dac1bb7925421dda72e468e519f6dbf1
luzejia <52991866+luzejia@users.noreply.github.com>
2021-09-01 10:57:56 +0800

    modify c d

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/luzejia/go-mod-test-b/helloworld"
+// import "github.com/luzejia/go-mod-test-b/helloworld"
+
+import helloworldC  "github.com/luzejia/go-mod-test-c/helloworld"
+import helloworldD  "github.com/luzejia/go-mod-test-d/helloworld"
 
 func main() {
-	helloworld.HelloWorldC()
-	helloworld.HelloWorldD()
+	// helloworld.HelloWorldC()
+	// helloworld.HelloWorldD()
+        helloworldC.HelloWorld()
+        helloworldD.HelloWorld()
 }
